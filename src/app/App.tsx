6cf0d5605abd7f800d62751733c90abe2d93f8b4
Banchee501 <rosstactical373@gmail.com>
2023-05-19 14:24:27 +0300

    add new lib and sructure file

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,9 @@
-import { Header } from './components/header/Header';
-import { TodoPanel } from './components/todoPanel/TodoPanel';
-import { TodoList } from './components/todoList/TodoList';
-import { TodoProvider } from './utils/contextes';
+import { Header } from '../components/header/Header';
+import { TodoPanel } from '../components/todoPanel/TodoPanel';
+import { TodoList } from '../components/todoList/TodoList';
+import { TodoProvider } from '../utils/contextes';
 
-import './styles/style.scss'
+import '../styles/style.scss'
 
 function App() {
 
