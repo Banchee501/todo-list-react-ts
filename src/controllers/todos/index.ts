c90c46b8603a5384011b435c568b1a39cb723746
Banchee501 <rosstactical373@gmail.com>
2023-06-12 19:27:40 +0300

    create API controllers

--- src/controllers/todos/index.ts
+++ src/controllers/todos/index.ts
@@ -1,1 +1,13 @@
-export { }
+import { Response, Request } from "express"
+import { ITodo } from "../../types/types"
+import Todo from "../../models/todo"
+
+const getTodos = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const todos: ITodo[] = await Todo.find()
+    res.status(200).json({ todos })
+  } catch (error) {
+    throw error
+  }
+}
+
